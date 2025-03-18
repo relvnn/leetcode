7a8b921372a42c8a0aa0e8c6b1789eaa22b5ce11
Relvnn 🜚 <relvnn7@gmail.com>
2025-03-18 01:28:16 +0000

    mergeTwoLists.py

---
+++ mergeTwoLists.py
@@ -0,0 +1,41 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        prehead = ListNode(-1)
+        prev = prehead
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+
+        prev.next = l1 if l1 is not None else l2
+        return prehead.next
+
+def print_linked_list(node):
+    while node:
+        print(node.val, end=" -> " if node.next else "")
+        node = node.next
+    print()
+
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(4)
+
+l2 = ListNode(1)
+l2.next = ListNode(3)
+l2.next.next = ListNode(4)
+
+solution = Solution()
+merged_list = solution.mergeTwoLists(l1, l2)
+
+print("Merged List:")
+print_linked_list(merged_list)
